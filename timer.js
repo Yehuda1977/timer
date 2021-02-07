0f99c2eef1ecc31b1ad2fc4b2bc47fd8c5b0d037
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-02-07 15:28:11 +0200

    Done for now.

--- timer.js
+++ timer.js
@@ -1,30 +1,64 @@
+// I learned how to make a progress ring from this article
+// https://css-tricks.com/building-progress-ring-quickly/
+
 const circle = document.querySelector('.progress-ring__circle');
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = circumference-(circumference*.0);
+circle.style.strokeDashoffset = circumference;
+let countDown;
+let alarm = new Audio("openhat.wav");
 
-const timer = (seconds) => {
-    let percentage = 0;
-    let tick = 1/seconds;
-    setInterval(()=>{
-        circle.style.strokeDashoffset = circumference-(circumference*percentage)
-        if(percentage<.9){
-
-            percentage += tick;
-           
-        }
-    
-    
-    }, 1000)
+const alarmSound = () => {
+    alarm.play();
 }
 
+
+
+
+const timer = (seconds) => {
+    
+    let percentage = 0;
+    let tick = 1/seconds;
+    
+    countDown = setInterval(()=>{
+        circle.style.strokeDashoffset = circumference-(circumference*percentage)
+        if(percentage<1){
+            percentage += tick;
+        } 
+        
+    }, 1000)
+    
+    
+    
+}
+
+const resetTimer = () => {
+    clearInterval(countDown);
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = circumference;
+}
+
+
+
+
 const seconds = document.querySelector('input');
+const container = document.querySelector('#container');
+container.style.width = '120px'
 
 
 
+let start = document.querySelector('#start');
+let reset = document.querySelector('#reset');
 
-let btn = document.createElement('button');
-document.body.appendChild(btn).innerHTML = 'Start';
-btn.addEventListener('click', () => timer(seconds.value))
+
+start.addEventListener('click', () => {
+    clearInterval(countDown);
+    timer(seconds.value);
+})
+
+
+reset.addEventListener('click', () => {
+    resetTimer();
+})
